e81dafbf8b8d991b563afdd570188cf727697d3e
Вадим <nerzhularthas@gmail.com>
2023-06-16 23:56:40 +0600

    Token types

---
+++ vxs-plsql-translator/src/main/java/org/vxs/lexer/TokenType.java
@@ -0,0 +1,11 @@
+package org.vxs.lexer;
+
+public class TokenType {
+    public final int none = 0;
+    public final int keyword = 1;
+    public final int operator = 2;
+    public final int value = 3;
+    public final int name = 4;
+    public final int special = 5;
+    public final int commentary = 6;
+}
