6532bcd5297fd0f0926d47d081f1f2a09b539984
Вадим <nerzhularthas@gmail.com>
2023-06-17 22:12:57 +0600

    Source

--- VXsPlSqlTranslator/ISource.cs
+++ VXsPlSqlTranslator/ISource.cs
@@ -1,3 +1,3 @@
 public interface ISource {
-    IEnumerable<char> Data();
+    IEnumerable<string> Data();
 }
--- VXsPlSqlTranslator/OracleSource.cs
+++ VXsPlSqlTranslator/OracleSource.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Oracle.ManagedDataAccess.Client;
 
 public class OracleSource : ISource {
@@ -7,16 +8,30 @@ public class OracleSource : ISource {
         ConnectionString = connectionString;
     }
 
-    public IEnumerable<char> Data() {
+    public IEnumerable<string> Data() {
         using var conn = new OracleConnection(ConnectionString);
         conn.Open();
         var cmd = conn.CreateCommand();
         cmd.CommandText = "select * from user_source where type not in ('JAVA SOURCE') order by type, name, line";
+        var sb = new StringBuilder();
+        string currentType = "";
+        string currentName = "";
         using var reader = cmd.ExecuteReader();
         while(reader.Read()) {
+            var type = reader["type"].ToString();
+            if (null == type) continue;
+            var name = reader["name"].ToString();
+            if (null == name) continue;
+            if (currentType != type || currentName != name) {
+                if (0 != sb.Length) yield return sb.ToString();    
+                sb.Clear();
+                currentType = type;
+                currentName = name;
+            }
             var text = reader["text"].ToString();
-            if (string.IsNullOrEmpty(text)) continue;
-            foreach(var c in text) yield return c;
+            if (null == text) continue;
+            sb.AppendLine(text);
         }
+        if (0 != sb.Length) yield return sb.ToString();
     }
 }
