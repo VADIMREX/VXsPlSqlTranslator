4e123e07aaf7dbf62058e63fd129b5d81b258701
Вадим <nerzhularthas@gmail.com>
2023-06-17 22:15:26 +0600

    Commentaries

--- VXsPlSqlTranslator/Lexer/PlSqlLexer.cs
+++ VXsPlSqlTranslator/Lexer/PlSqlLexer.cs
@@ -1,26 +1,137 @@
+using System.Text;
+
 namespace VXs.Lexer;
 
-public class PlSqlLexer {
-    
-    void Token InlineComment(IEnumerator<char> enumerator) {
-        
+public class PlSqlLexer
+{
+    class Position
+    {
+        public int i = 0;
+        public int pos = 0;
+        public int line = 0;
+        public int col = 0;
+
+        public int next => i + 1;
+
+        public static Position operator ++(Position self)
+        {
+            self.i++;
+            self.pos++;
+            self.col++;
+            return self;
+        }
+
+        public static Position operator +(Position self, int arg)
+        {
+            self.i += arg;
+            self.pos += arg;
+            self.col += arg;
+            return self;
+        }
+
+        public static Position operator -(Position self, int arg)
+        {
+            self.i -= arg;
+            self.pos -= arg;
+            self.col -= arg;
+            return self;
+        }
+
+        public static Position operator --(Position self)
+        {
+            self.i--;
+            self.pos--;
+            self.col--;
+            return self;
+        }
+
+        public static Position operator -(Position self)
+        {
+            self.i++;
+            self.pos++;
+            self.line++;
+            self.col = 0;
+            return self;
+        }
+
+        public static implicit operator int(Position self) => self.i;
     }
-    
-    public IEnumerable<Token> Parse(IEnumerable<char> source) {
-        var pos = -1;
-        var line = 0;
-        var col = -1;
-        var enumerator = source.GetEnumerator();
-        
-        while(enumerator.MoveNext()) {
-            var c = enumerator.Current;
-            switch(c) {
+
+    Token? InlineComment(Position pos, string source)
+    {
+        if (pos.next >= source.Length) return new Token(TokenType.Operator, pos.line, pos.col, pos.i, "-");
+
+        if ('-' != source[pos.next])
+        {
+            pos--;
+            return new Token(TokenType.Operator, pos.line, pos.col, pos.i, "-");
+        }
+
+        var buffer = new StringBuilder();
+        for (pos += 2; pos < source.Length; pos++)
+        {
+            var c = source[pos];
+            if ('\n' == c)
+            {
+                break;
+            }
+            buffer.Append(c);
+        }
+        return new Token(TokenType.Commentary, "inline", pos.line, pos.col, pos.i, buffer.ToString());
+    }
+
+    Token? MultilineComment(Position pos, string source) {
+        if (pos.next >= source.Length) return new Token(TokenType.Operator, pos.line, pos.col, pos.i, "/");
+
+        if ('*' != source[pos.next])
+        {
+            pos--;
+            return new Token(TokenType.Operator, pos.line, pos.col, pos.i, "/");
+        }
+
+        var buffer = new StringBuilder();
+        for (pos += 2; pos < source.Length; pos++)
+        {
+            var c = source[pos];
+            if ('*' == c)
+            {
+                if (pos.next >= source.Length) break;
+                if ('/' == source[pos.next])
+                {
+                    pos++;
+                    return new Token(TokenType.Commentary, "multiline", pos.line, pos.col, pos.i, buffer.ToString());
+                }
+            }
+            buffer.Append(c);
+        }
+        return new Token(TokenType.Error, pos.line, pos.col, pos.i, "unclosed commentary");
+    }
+
+    public IEnumerable<Token> Parse(string source)
+    {
+        var pos = new Position();
+        Token? token = null;
+        for (; pos < source.Length; pos++)
+        {
+            var c = source[pos];
+            switch (c)
+            {
                 case '+':
                 case '*':
-                    yield return new Token(TokenType.Operator, line, col, pos, $"{c}");
+                    token = new Token(TokenType.Operator, pos.line, pos.col, pos.pos, $"{c}");
                     break;
                 case '-':
-                    ParseInlineComment(IEnumerator<char>)
+                    token = InlineComment(pos, source);
+                    break;
+                case '/':
+                    token = MultilineComment(pos, source);
+                    break;
+            }
+
+            if (null != token)
+            {
+                yield return token;
+                token = null;
             }
         }
     }
