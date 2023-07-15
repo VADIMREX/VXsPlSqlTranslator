26ed4fc85546e3350015884416a8faf9a3f97ac8
Вадим <nerzhularthas@gmail.com>
2023-07-15 22:24:09 +0600

    Rework of expressions

--- VXsPlSqlTranslator/Parser/PlSql/PlSqlExpression.cs
+++ VXsPlSqlTranslator/Parser/PlSql/PlSqlExpression.cs
@@ -9,75 +9,128 @@ public class PlSqlExpression : AstNodeParser
 
     protected override void Parse(IEnumerator<Token> enumerator) => ParseCurrent(enumerator);
 
-    protected virtual (int, StateResult) State0(IEnumerator<Token> enumerator)
-    {
+    Token save;
+    protected virtual (int, StateResult) ParseKeyword(IEnumerator<Token> enumerator) {
         var token = enumerator.Current;
-        switch (token.Type)
-        {
-            case TokenType.Keyword:
-                goto case TokenType.Name;
-            case TokenType.Name:
-                AddChild(new PlSqlName(enumerator));
-                return State1(enumerator);
-            case TokenType.Value:
-                AddChild(new AstNode(token, "value"));
-                return (1, StateResult.Continue);
-            case TokenType.Special:
-                if ("(" == token.Text)
-                {
-                    return (0, StateResult.Continue);
-                }
-                goto case TokenType.Operator;
-            case TokenType.Operator:
-                return State1(enumerator);
-        }
-#warning todo error
-        return (-1, StateResult.Return);
-    }
-
-    protected virtual (int, StateResult) State1(IEnumerator<Token> enumerator)
-    {
-        var token = enumerator.Current;
-        if (EndType == token.Type)
-        {
-            var text = TokenType.Keyword == token.Type ? token.GetPlSqlText() : token.Text;
-            if (EndText == text)
-                return (-1, StateResult.Return);
-        }
-        switch (token.Type)
-        {
-            case TokenType.Keyword:
-                if ("END" == token.GetPlSqlText())
-                    return (-1, StateResult.Return);
-                goto case TokenType.Operator;
-            case TokenType.Special:
-                switch (token.Text)
-                {
-                    case "(":
-                        AddChild(new PlSqlExpression(enumerator));
-                        return (0, StateResult.Continue);
-                    case ")":
-                        return (-1, StateResult.Return);
-                    case ",":
-                        AddChild(new AstNode(token, "operator"));
-                        return (0, StateResult.Continue);
-                    case ";":
-                        return (-1, StateResult.Return);
-                }
-#warning todo error
-                return (-1, StateResult.Return);
-            case TokenType.Operator:
+        switch(token.GetPlSqlText()) {
+            case "NULL":
+                AddChild(new AstNode(token, "null"));
+                return (0, StateResult.Continue);
+            case "IN":
                 AddChild(new AstNode(token, "operator"));
                 return (0, StateResult.Continue);
+            case "IS":
+                AddChild(new AstNode(token, "operator"));
+                return (1, StateResult.Continue);
+            default:
+                return (0, StateResult.Continue);
         }
-#warning todo error
-        return (-1, StateResult.Return);
+    }
+
+    protected virtual (int, StateResult) ParseName(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        AddChild(new AstNode(token, "name"));
+        return (0, StateResult.Continue);
+    }
+
+    protected virtual (int, StateResult) ParseValue(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        AddChild(new AstNode(token, "value"));
+        return (0, StateResult.Continue);
+    }
+
+    protected virtual (int, StateResult) ParseSpecial(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        switch(token.GetPlSqlText()) {
+            case ";":
+                // is need to add to tree?
+                return (-1, StateResult.Return);
+            case "(":
+                enumerator.MoveNext();
+                AddChild(new PlSqlExpression(enumerator, TokenType.Special, ")"));
+                return (-1, StateResult.Return);
+            case ")":
+                AddChild(new AstNode(token, "null"));
+                return (-1, StateResult.Return);
+            default:
+                return (0, StateResult.Continue);
+        }
+    }
+
+    protected virtual (int, StateResult) ParseOperator(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        AddChild(new AstNode(token, "operator"));
+        return (0, StateResult.Continue);
+    }
+    
+    /// <summary><summary>
+    protected virtual (int, StateResult) State0(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        var state = -1;
+        var stateResult = StateResult.Return;
+        if (EndType == token.Type && EndText == token.GetPlSqlText())
+        {
+            return (state, stateResult);
+        }
+        switch (token.Type)
+        {
+            case TokenType.Keyword:
+                (state, stateResult) = ParseKeyword(enumerator);
+                break;
+            case TokenType.Name:
+                (state, stateResult) = ParseName(enumerator);
+                break;
+            case TokenType.Value:
+                (state, stateResult) = ParseValue(enumerator);
+                break;
+            case TokenType.Special:
+                (state, stateResult) = ParseSpecial(enumerator);
+                break;
+            case TokenType.Operator:
+                (state, stateResult) = ParseOperator(enumerator);
+                break;
+        }
+        return (state, stateResult);
+    }
+
+    protected virtual (int, StateResult) State1(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        if (TokenType.Keyword != token.Type) {
+            #warning error
+            return (-1, StateResult.Return);
+        }
+        switch(token.GetPlSqlText()) {
+            case "NULL":
+                AddChild(new AstNode(token, "null"));
+                return (0, StateResult.Continue);
+            case "NOT":
+                ((AstNode)(Childs[Childs.Count - 1])).AddChild(new AstNode(token, "operator"));
+                return (2, StateResult.Continue);
+            default:
+                #warning error
+                return (-1, StateResult.Return);
+        }
+    }
+
+    protected virtual (int, StateResult) State2(IEnumerator<Token> enumerator) {
+        var token = enumerator.Current;
+        if (TokenType.Keyword != token.Type) {
+            #warning error
+            return (-1, StateResult.Return);
+        }
+        if ("NULL" != token.GetPlSqlText()) {
+            #warning error
+            return (-1, StateResult.Return);
+        }
+        AddChild(new AstNode(token, "null"));
+        return (0, StateResult.Continue);            
     }
 
     protected override void InitStates()
     {
         stateActions.Add(State0);
         stateActions.Add(State1);
+        stateActions.Add(State2);
     }
 
     public PlSqlExpression(IEnumerator<Token> enumerator, TokenType endType = TokenType.None, string endText = "") : base(enumerator.Current, "expression")
